9cfa9abac40671641370728fd15d704053775bee
Emil Pettersson <emil.pettersson05@gmail.com>
2017-12-04 21:37:30 +0200

    Add TodoSearch component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import TodoSearch from 'TodoSearch';
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 
 export default class TodoApp extends React.Component {
     constructor (props) {
         super(props);
+        this.handleSearch = this.handleSearch.bind(this);
         this.state = {
+            showCompleted: false,
+            searchText: '',
             todos: [
                 {
                     id: 1,
@@ -22,11 +26,17 @@ export default class TodoApp extends React.Component {
     handleAddTodo (text) {
         alert(`New todo: ${text}`);
     }
+    handleSearch (showCompleted, searchText) {
+        this.setState({
+            showCompleted: showCompleted,
+            searchText: searchText.toLowerCase()
+        });
+    }
     render () {
         var {todos} = this.state;
         return (
             <div>
-                TodoApp Component
+                <TodoSearch onSearch={this.handleSearch}/>
                 <TodoList todos={todos} />
                 <AddTodo onAddTodo={this.handleAddTodo}/>
             </div>
---
+++ app/components/TodoSearch.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class TodoSearch extends React.Component {
+    constructor (props) {
+        super(props);
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+    handleSearch () {
+        const showCompleted = this.refs.showCompleted.checked;
+        const searchText = this.refs.searchText.value;
+
+        this.props.onSearch(showCompleted, searchText);
+    }
+    render () {
+        return (
+            <div>
+                <div>
+                    <input type="search" ref="searchText" placeholder="Search todos" onChange={this.handleSearch}/>
+                </div>
+                <div>
+                    <label>
+                        <input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
+                        Show completed todos
+                    </label>
+                </div>
+            </div>
+        )
+    }
+}
